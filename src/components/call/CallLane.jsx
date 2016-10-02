d6acc93d405f5893c1a0984ebd94d3cf03be9093
Richard Olsson <r@richardolsson.se>
2016-10-02 16:15:39 +0200

    Add basic CallPage with placeholder CallLane

---
+++ src/components/call/CallLane.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default class CallLane extends React.Component {
+    render() {
+        return (
+            <h1>CallLane</h1>
+        );
+    }
+}
---
+++ src/components/pages/CallPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import CallLane from '../call/CallLane';
+
+
+export default class CallPage extends React.Component {
+    render() {
+        return (
+            <CallLane/>
+        );
+    }
+}

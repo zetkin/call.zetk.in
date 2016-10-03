5423aeb18dd53a7da074ae8d3a0ce7bd3798ae94
Richard Olsson <r@richardolsson.se>
2016-10-03 17:51:05 +0200

    Add buttons to toggle info mode in instructions/target panes

--- src/components/panes/TargetPane.jsx
+++ src/components/panes/TargetPane.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { FormattedMessage as Msg } from 'react-intl';
 
 import PaneBase from './PaneBase';
 import { currentCall } from '../../store/calls';
+import { setLaneInfoMode } from '../../actions/lane';
 
 
 const mapStateToProps = state => ({
@@ -17,5 +19,25 @@ export default class TargetPane extends PaneBase {
         return [
             <h1 key="name">{ target.get('name') }</h1>
         ];
+    }
+
+    renderHeader() {
+        let step = this.props.step;
+
+        if (step === 'call' || step === 'report') {
+            return (
+                <button className="TargetPane-instructionsButton"
+                    onClick={ this.onClickInstructions.bind(this) }>
+                    <Msg id="panes.target.instructions"/>
+                </button>
+            );
+        }
+        else {
+            return null;
+        }
+    }
+
+    onClickInstructions() {
+        this.props.dispatch(setLaneInfoMode('instructions'));
     }
 }

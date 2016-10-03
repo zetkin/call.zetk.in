6760a054c83d483c88668b3f5f74b75615ba9626
Richard Olsson <r@richardolsson.se>
2016-10-03 13:48:29 +0200

    Add basic content with placeholder stats to AssignmentPane

--- src/components/panes/AssignmentPane.jsx
+++ src/components/panes/AssignmentPane.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import PaneBase from './PaneBase';
+import { selectedAssignment } from '../../store/assignments';
 
 
+const mapStateToProps = state => ({
+    assignment: selectedAssignment(state),
+});
+
+@connect(mapStateToProps)
 export default class AssignmentPane extends PaneBase {
     renderContent() {
-        return <h1>AssignmentPane</h1>;
+        let assignment = this.props.assignment;
+
+        return [
+            <h1 key="title">{ assignment.get('title') }</h1>,
+            <p key="desc">
+                { assignment.get('description') }
+            </p>,
+            <div key="stats" className="AssignmentPane-stats">
+                Stats go here.
+            </div>
+        ];
     }
 }

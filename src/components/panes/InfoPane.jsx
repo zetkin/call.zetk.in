534bba5a9780c2370d4bc7fc7d1265d22db369c3
Richard Olsson <r@richardolsson.se>
2016-10-03 13:48:52 +0200

    Add instructions to info pane

--- src/components/panes/InfoPane.jsx
+++ src/components/panes/InfoPane.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { FormattedMessage as Msg } from 'react-intl';
+import { connect } from 'react-redux';
 
 import PaneBase from './PaneBase';
+import { selectedAssignment } from '../../store/assignments';
 
 
+const mapStateToProps = state => ({
+    assignment: selectedAssignment(state),
+});
+
+@connect(mapStateToProps)
 export default class InfoPane extends PaneBase {
     renderContent() {
-        return <h1>InfoPane</h1>;
+        let assignment = this.props.assignment;
+
+        return [
+            <Msg tagName="h1" key="h1" id="panes.info.h1"/>,
+            <div key="instructions" className="InfoPane-instructions"
+                dangerouslySetInnerHTML={{
+                    __html: assignment.get('instructions') }}/>,
+        ];
     }
 }

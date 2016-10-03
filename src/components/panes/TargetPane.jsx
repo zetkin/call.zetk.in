14509ea2d1dfe813db0844477316856b3efdb1df
Richard Olsson <r@richardolsson.se>
2016-10-03 17:25:06 +0200

    Hook up TargetPane to store
    Allows to retrieve target data, which is really empty right now. API needs to
    expose more data before it makes sense to build this pane.

--- src/components/panes/TargetPane.jsx
+++ src/components/panes/TargetPane.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import PaneBase from './PaneBase';
+import { currentCall } from '../../store/calls';
 
 
+const mapStateToProps = state => ({
+    call: currentCall(state),
+});
+
+@connect(mapStateToProps)
 export default class TargetPane extends PaneBase {
     renderContent() {
-        return <h1>TargetPane</h1>;
+        let target = this.props.call.get('target');
+
+        return [
+            <h1 key="name">{ target.get('name') }</h1>
+        ];
     }
 }

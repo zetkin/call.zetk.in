6a7d76d2bf986bc255307742fbbac8d838fe867d
Richard Olsson <r@richardolsson.se>
2016-10-05 13:50:32 +0200

    Don't re-render report pane while it's leaving

--- src/components/panes/ReportPane.jsx
+++ src/components/panes/ReportPane.jsx
@@ -14,6 +14,24 @@ const mapStateToProps = state => ({
 
 @connect(mapStateToProps)
 export default class ReportPane extends PaneBase {
+    shouldComponentUpdate(nextProps) {
+        let step = this.props.step;
+        let call = this.props.call;
+        let nextCall = nextProps.call;
+
+        if (step === 'done' && !nextCall.get('report')) {
+            // If we are in the "done" step, about to start a new call, and
+            // that call does not yet have a report, don't re-render. It just
+            // means that the pane is animating out and will be removed in a
+            // moment. The next time a report is due, the pane will be created
+            // anew, the step will be "report", and it will render properly.
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
     renderContent() {
         let call = this.props.call;
         let report = call.get('report');

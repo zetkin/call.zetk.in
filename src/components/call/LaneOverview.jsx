9a4b3c52ba1e9484d96fe3bef4cce8234ceb8ca9
Richard Olsson <r@richardolsson.se>
2016-10-07 11:56:02 +0200

    Add lane overview with placeholder list of old calls

--- src/components/call/LaneOverview.jsx
+++ src/components/call/LaneOverview.jsx
@@ -1,12 +1,66 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import LoadingIndicator from '../misc/LoadingIndicator';
+import { retrieveUserCalls } from '../../actions/call';
 
 
+const mapStateToProps = state => ({
+    callList: state.getIn(['calls', 'callList']),
+});
+
+@connect(mapStateToProps)
 export default class LaneOverview extends React.Component {
+    componentDidMount() {
+        this.props.dispatch(retrieveUserCalls());
+    }
+
     render() {
+        let callList = this.props.callList;
+        let content = null;
+
+        if (callList.get('isPending')) {
+            content = <LoadingIndicator/>;
+        }
+        else if (callList.get('error')) {
+            content = <h1>ERROR</h1>;
+        }
+        else if (callList.get('items')) {
+            content = (
+                <ul className="LaneOverview-log">
+                { callList.get('items').toList().map(call => (
+                    <CallLogItem key={ call.get('id') }
+                        onSelect={ this.onStartNewCall.bind(this, call) }
+                        call={ call }/>
+                )) }
+                </ul>
+            );
+        }
+        else {
+            content = null;
+        }
+
         return (
             <div className="LaneOverview">
-                Placeholder
+                { content }
             </div>
         );
     }
+
+    onStartNewCall(oldCall) {
+        console.log('Re-call', oldCall.get('target').get('name'));
+    }
 }
+
+
+const CallLogItem = props => {
+    let call = props.call;
+
+    return (
+        <li className="LaneOverview-logItem">
+            <a onClick={ props.onSelect }>
+                { call.getIn(['target', 'name']) }
+            </a>
+        </li>
+    );
+};

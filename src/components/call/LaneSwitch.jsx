eb584cfc5106a99e1259d318ec02a9d4616ebbd6
Richard Olsson <r@richardolsson.se>
2016-10-08 17:29:07 +0200

    Switch between lanes from LaneSwitch

--- src/components/call/LaneSwitch.jsx
+++ src/components/call/LaneSwitch.jsx
@@ -3,9 +3,13 @@ import { connect } from 'react-redux';
 
 import PropTypes from '../../utils/PropTypes';
 import { setCallViewState } from '../../actions/view';
+import { switchLaneToCall } from '../../actions/lane';
+import { activeCalls, currentCall } from '../../store/calls';
 
 
 const mapStateToProps = state => ({
+    activeCalls: activeCalls(state),
+    currentCall: currentCall(state),
     viewState: state.getIn(['view', 'callViewState']),
 });
 
@@ -17,11 +21,21 @@ export default class LaneSwitch extends React.Component {
         let content;
 
         if (viewState === 'lane') {
-            // TODO: Show active calls
-            content = (
-                <a className="LaneSwitch-openLogButton"
-                    onClick={ this.onClickOpen.bind(this) }>log</a>
-            );
+            let otherCalls = this.props.activeCalls.filter(call =>
+                call !== this.props.currentCall);
+
+            content = [
+                <a key="openButton" className="LaneSwitch-openLogButton"
+                    onClick={ this.onClickOpen.bind(this) }>log</a>,
+                <ul key="callList" className="LaneSwitch-callList">
+                { otherCalls.map(call => (
+                    <li key={ call.get('id') }
+                        onClick={ this.onClickOtherCall.bind(this, call) }>
+                        { call.get('target').get('name') }
+                    </li>
+                )) }
+                </ul>
+            ];
         }
         else {
             content = (
@@ -35,6 +49,10 @@ export default class LaneSwitch extends React.Component {
                 { content }
             </div>
         );
+    }
+
+    onClickOtherCall(call) {
+        this.props.dispatch(switchLaneToCall(call));
     }
 
     onClickOpen() {

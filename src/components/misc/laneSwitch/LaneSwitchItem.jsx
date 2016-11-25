74b7193f0fcf251bc0127b5305034749c20ff07b
Richard Olsson <r@richardolsson.se>
2016-11-25 11:34:13 +0100

    Add "jump" animation to LaneSwitch items
    This is meant to make sure that the caller does not forget that there are more
    calls allocated for them.

--- src/components/misc/laneSwitch/LaneSwitchItem.jsx
+++ src/components/misc/laneSwitch/LaneSwitchItem.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import cx from 'classnames';
 
 import Avatar from '../Avatar';
 
 
 export default class LaneSwitchItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            jumping: false,
+        };
+    }
+
+    componentDidMount() {
+        let delay = 1000 + Math.random() * 4000;
+        this.timer = setTimeout(this.onTimeout.bind(this), delay);
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timer);
+    }
+
     render() {
         let call = this.props.call;
         let target = call.get('target');
 
+        let classes = cx('LaneSwitchItem', {
+            jumping: this.state.jumping,
+        });
+
         return (
-            <li className="LaneSwitchItem"
+            <li className={ classes }
                 title={ target.get('name') }
                 onClick={ this.props.onClick }>
                 <Avatar personId={ target.get('id') }
@@ -18,5 +40,24 @@ export default class LaneSwitchItem extends React.Component {
                     />
             </li>
         );
+    }
+
+    onTimeout() {
+        if (this.state.jumping) {
+            // Was jumping. Wait 2-10 seconds before jumping again.
+            let delay = 2000 + Math.random() * 8000;
+            this.timer = setTimeout(this.onTimeout.bind(this), delay);
+
+            this.setState({
+                jumping: false,
+            });
+        }
+        else {
+            // Should jump, then wait 500 ms (at which it will be reset).
+            this.timer = setTimeout(this.onTimeout.bind(this), 1000);
+            this.setState({
+                jumping: true,
+            });
+        }
     }
 }

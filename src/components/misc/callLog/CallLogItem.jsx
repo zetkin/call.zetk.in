6ca8edd2a143a90b582ada9f13a420c4c327d118
Richard Olsson <r@richardolsson.se>
2016-12-02 16:16:10 +0100

    Add expandable log notes to CallLogItem

--- src/components/misc/callLog/CallLogItem.jsx
+++ src/components/misc/callLog/CallLogItem.jsx
@@ -1,46 +1,88 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { FormattedDate, FormattedMessage as Msg } from 'react-intl';
 import cx from 'classnames';
 
 
-export default function CallLogItem(props) {
-    let call = props.call;
-    let state = call.get('state');
+export default class CallLogItem extends React.Component {
+    constructor(props) {
+        super(props);
 
-    let classes = cx('CallLogItem', 'status' + state);
-    let allocTime = new Date(call.get('allocation_time'));
+        this.state = {
+            viewMode: null,
+        };
+    }
 
-    let summaryMsg = 'misc.callLog.summary.status' + state;
-    let summaryValues = {
-        caller: call.getIn(['caller', 'name']),
-        target: call.getIn(['target', 'name']),
-    };
+    componentDidMount() {
+        let node = ReactDOM.findDOMNode(this.refs.notes);
+        if (node.clientHeight > 25) {
+            this.setState({
+                viewMode: 'contracted',
+            });
+        }
+    }
 
-    return (
-        <div className={ classes }>
-            <div className="CallLogItem-status">
-                <span className="CallLogItem-statusBar0"/>
-                <span className="CallLogItem-statusBar1"/>
+    render() {
+        let call = this.props.call;
+        let state = call.get('state');
+
+        let allocTime = new Date(call.get('allocation_time'));
+
+        let summaryMsg = 'misc.callLog.summary.status' + state;
+        let summaryValues = {
+            caller: call.getIn(['caller', 'name']),
+            target: call.getIn(['target', 'name']),
+        };
+
+        let classes = cx('CallLogItem', 'status' + state, {
+            contracted: this.state.viewMode === 'contracted',
+            expanded: this.state.viewMode === 'expanded',
+        });
+
+        let expandButton = null;
+        if (this.state.viewMode) {
+            expandButton = (
+                <button onClick={ this.onExpandButtonClick.bind(this) }>
+                    Expand</button>
+            );
+        }
+
+        return (
+            <div className={ classes }>
+                <div className="CallLogItem-status">
+                    <span className="CallLogItem-statusBar0"/>
+                    <span className="CallLogItem-statusBar1"/>
+                </div>
+                <div className="CallLogItem-timeStamp">
+                    <FormattedDate value={ allocTime }
+                        weekday="short"
+                        day="numeric"
+                        month="short"
+                        year="numeric"
+                        hour="2-digit"
+                        minute="2-digit"
+                        />
+                </div>
+                <div className="CallLogItem-summary">
+                    <Msg id={ summaryMsg }
+                        values={ summaryValues }
+                        />
+                </div>
+                <div ref="notes"
+                    className="CallLogItem-notes">
+                    { call.get('notes') }
+                </div>
+                { expandButton }
+                <div className="CallLogItem-caller">
+                    { call.getIn(['caller', 'name']) }</div>
             </div>
-            <div className="CallLogItem-timeStamp">
-                <FormattedDate value={ allocTime }
-                    weekday="short"
-                    day="numeric"
-                    month="short"
-                    year="numeric"
-                    hour="2-digit"
-                    minute="2-digit"
-                    />
-            </div>
-            <div className="CallLogItem-summary">
-                <Msg id={ summaryMsg }
-                    values={ summaryValues }
-                    />
-            </div>
-            <div className="CallLogItem-notes">
-                { call.get('notes') }</div>
-            <div className="CallLogItem-caller">
-                { call.getIn(['caller', 'name']) }</div>
-        </div>
-    );
+        );
+    }
+
+    onExpandButtonClick() {
+        this.setState({
+            viewMode: this.state.viewMode === 'contracted'?
+                'expanded' : 'contracted',
+        });
+    }
 }

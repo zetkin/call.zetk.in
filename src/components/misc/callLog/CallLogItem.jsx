959ffd2240f1adebc2ce4b72e54cc1426a3d4e79
Richard Olsson <r@richardolsson.se>
2016-12-02 14:11:03 +0100

    Add call status bar

--- src/components/misc/callLog/CallLogItem.jsx
+++ src/components/misc/callLog/CallLogItem.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import cx from 'classnames';
 
 
 export default function CallLogItem(props) {
     let call = props.call;
+    let state = call.get('state');
+
+    let classes = cx('CallLogItem', 'status' + state);
 
     return (
-        <div className="CallLogItem">
+        <div className={ classes }>
+            <div className="CallLogItem-status">
+                <span className="CallLogItem-statusBar0"/>
+                <span className="CallLogItem-statusBar1"/>
+            </div>
             <div className="CallLogItem-timeStamp">
                 { call.get('allocation_time') }</div>
             <div className="CallLogItem-summary">

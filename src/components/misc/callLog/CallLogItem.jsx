f21415eb11e35a296f7521822d2d95e2da03af3e
Richard Olsson <r@richardolsson.se>
2016-12-02 14:32:44 +0100

    Replace ISO timestamp with formatted date

--- src/components/misc/callLog/CallLogItem.jsx
+++ src/components/misc/callLog/CallLogItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FormattedDate } from 'react-intl';
 import cx from 'classnames';
 
 
@@ -7,6 +8,7 @@ export default function CallLogItem(props) {
     let state = call.get('state');
 
     let classes = cx('CallLogItem', 'status' + state);
+    let allocTime = new Date(call.get('allocation_time'));
 
     return (
         <div className={ classes }>
@@ -15,7 +17,15 @@ export default function CallLogItem(props) {
                 <span className="CallLogItem-statusBar1"/>
             </div>
             <div className="CallLogItem-timeStamp">
-                { call.get('allocation_time') }</div>
+                <FormattedDate value={ allocTime }
+                    weekday="short"
+                    day="numeric"
+                    month="short"
+                    year="numeric"
+                    hour="2-digit"
+                    minute="2-digit"
+                    />
+            </div>
             <div className="CallLogItem-summary">
                 { call.get('notes') }</div>
             <div className="CallLogItem-caller">

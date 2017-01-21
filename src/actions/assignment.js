9f58f7c18672efae6422fa307e3aef897354c321
Richard Olsson <r@richardolsson.se>
2017-01-21 07:56:23 +0100

    Don't retrieve old assignments

--- src/actions/assignment.js
+++ src/actions/assignment.js
@@ -3,10 +3,17 @@ import * as types from '.';
 
 export function retrieveUserAssignments() {
     return ({ dispatch, z }) => {
+        let today = (new Date()).format('{yyyy}{MM}{dd}')
+        let filters = [
+            ['start_date', '<', today],
+            ['end_date', '>', today],
+        ];
+
         dispatch({
             type: types.RETRIEVE_USER_ASSIGNMENTS,
             payload: {
-                promise: z.resource('users', 'me', 'call_assignments').get()
+                promise: z.resource('users', 'me', 'call_assignments')
+                    .get(null, null, filters),
             }
         });
     };

28f4502baa57e97456da9e6d771c91a164a05081
Richard Olsson <r@richardolsson.se>
2017-04-20 16:17:49 +0200

    Add summary of survey responses in SummaryStep

--- src/components/report/steps/SummaryStep.jsx
+++ src/components/report/steps/SummaryStep.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { FormattedMessage as Msg } from 'react-intl';
+import { toggleSurveyIncluded } from '../../../actions/survey';
 
 
+const mapStateToProps = (state, props) => ({
+    surveyList: state.getIn(['surveys', 'surveyList']),
+    surveyResponses: state.getIn(['surveys', 'pendingResponsesByCall', props.call.get('id')]),
+});
+
+@connect(mapStateToProps)
 export default class SummaryStep extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
-        // TODO: Replace with summary of actions during call
-        return null;
+        if (this.props.surveyResponses) {
+            let surveyItems = this.props.surveyResponses.map((sub, key) => {
+                let surveyId = key.toString();
+                let survey = this.props.surveyList.getIn(['items', surveyId]);
+                let included = sub.get('included');
+
+                return (
+                    <li key={ surveyId } className="SummaryStep-surveyItem">
+                        <input type="checkbox" checked={ included }
+                            onChange={ this.onSurveyToggle.bind(this, surveyId) }
+                            />
+                        <label>{ survey.get('title') }</label>
+                    </li>
+                );
+            }).toList();
+
+            return (
+                <div className="SummaryStep">
+                    <Msg tagName="p"
+                        id="report.steps.summary.surveys.status"
+                        values={{ count: surveyItems.size }}/>
+                    <ul className="SummaryStep-surveyList">
+                        { surveyItems }
+                    </ul>
+                </div>
+            );
+        }
+        else {
+            return null;
+        }
+    }
+
+    onSurveyToggle(surveyId, ev) {
+        this.props.dispatch(toggleSurveyIncluded(surveyId, ev.target.checked));
     }
 }

bf1874ef2b3d7417647b8546ce3ca4657ff2fee2
Victor Wihlborg <jag@vbw.se>
2018-06-11 14:18:50 +0200

    Add CurrentIsPending to skip call

--- src/components/overlays/SkipOverlay.jsx
+++ src/components/overlays/SkipOverlay.jsx
@@ -8,6 +8,7 @@ import { skipCall } from '../../actions/call';
 
 
 const mapStateToProps = state => ({
+    currentIsPending: state.getIn(['calls', 'currentIsPending'])
 });
 
 @connect(mapStateToProps)
@@ -18,14 +19,16 @@ export default class SkipOverlay extends React.Component {
         let targetName = target.get('name');
 
         return (
-            <div className="ResumeOverlay">
+            <div className="SkipOverlay">
                 <Msg tagName="h1" id="overlays.skip.h"
                     values={{ target: targetName }}/>
                 <Msg tagName="p" id="overlays.skip.p"
                     values={{ target: targetName }}/>
 
                 <Button labelMsg="overlays.skip.skipButton"
+                    className="SkipOverlay-skipButton"
                     labelValues={{ target: targetName }}
+                    loading={this.props.currentIsPending}
                     onClick={ this.onSkipButtonClick.bind(this) }
                     />
                 <Button labelMsg="overlays.skip.resumeButton"

396d01dd247fa23c8668b550c0a1d07458c20c66
Richard Olsson <r@richardolsson.se>
2018-06-12 16:52:20 +0200

    Add API credentials for dev environment

--- bin/run_dev.cmd
+++ bin/run_dev.cmd
@@ -7,8 +7,8 @@ docker run ^
     -v %cd%/src:/var/app/src ^
     --name call_zetk_in ^
     --env ZETKIN_LOGIN_URL=http://login.dev.zetkin.org ^
-    --env ZETKIN_APP_ID=a5 ^
-    --env ZETKIN_APP_KEY=abc123 ^
+    --env ZETKIN_APP_ID=514d9750b6f242c2a7abc31f6fc17a73 ^
+    --env ZETKIN_APP_KEY=NjcwOWU0ZDktOGVmYi00N2ViLTk1MTEtY2UyYjY0ODU5YWU4 ^
     --env ZETKIN_DOMAIN=dev.zetkin.org ^
     -p 80:80 ^
     -p 81:81 ^

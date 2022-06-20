9be33221b0c91cc71ef8b11256ee1a0366fa2a98
Richard Olsson <r@richardolsson.se>
2022-06-20 10:30:42 +0200

    Add profile attribute to indicate that VoIP credentials exist

--- src/actions/user.js
+++ src/actions/user.js
@@ -6,7 +6,25 @@ export function retrieveUserMemberships() {
         dispatch({
             type: types.RETRIEVE_USER_MEMBERSHIPS,
             payload: {
-                promise: z.resource('users', 'me', 'memberships').get()
+                promise: z.resource('users', 'me', 'memberships')
+                    .get()
+                    .then(res => {
+                        return Promise.all(res.data.data.map(membership => Promise.resolve(membership)
+                            .then(membership => {
+                                return z.resource('orgs', membership.organization.id, 'people', membership.profile.id)
+                                    .get()
+                                    .then(profileRes => {
+                                        const profileData = profileRes.data.data;
+                                        // Look for special custom fields for telavox user credentials to assess
+                                        // whether to use VoIP for this caller profile.
+                                        membership.profile.has_voip_credentials = (
+                                            !!profileData.telavox_username && !!profileData.telavox_password
+                                        );
+                                    });
+                            })
+                        ))
+                        .then(() => res);
+                    })
             }
         });
     };

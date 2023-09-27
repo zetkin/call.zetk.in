31624dcd11b7ec4116cbe7f248f99018cca1305a
Niklas Vanhainen <niklas.vanhainen@gmail.com>
2023-09-27 14:08:36 +0200

    Implement a psuedo-static map with Leaflet that gets coordinates from address using OSM/Nominatim

---
+++ src/components/misc/StaticMap.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+export default class StaticMap extends React.Component {
+    static propTypes = {
+        address: React.PropTypes.string,
+        zoom: React.PropTypes.number,
+    }
+
+    componentDidMount() {
+        const coordinatesPromise = this.getCoordinates();
+        this.loadLeafletScript().then(() => {
+            return coordinatesPromise;
+        })
+        .then((coordinates) => {
+            console.log(coordinates)
+            this.initMap(coordinates)
+        })
+    }
+
+    getCoordinates() {
+        const address = this.props.address;
+        return fetch(`https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(address)}&format=json&limit=1`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                if (data.length > 0) {
+                    const { lat, lon } = data[0];
+                    return [parseFloat(lat), parseFloat(lon)];
+                }
+            })
+            .catch((err) => {
+                console.log('Error: Address not found!');
+            })
+    }
+
+    initMap(coordinates) {
+        const map = L.map('map', {
+            zoomControl: false,
+            dragging: false,
+            touchZoom: false,
+            doubleClickZoom: false,
+            scrollWheelZoom: false,
+            boxZoom: false,
+            keyboard: false,
+        }).setView(coordinates, this.props.zoom);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+    }
+
+    loadLeafletScript() {
+        return new Promise((resolve, reject) => {
+            if (typeof L === 'undefined') {
+                const link = document.createElement('link');
+                link.rel = 'stylesheet';
+                link.href = 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css';
+                document.head.appendChild(link);
+
+                const script = document.createElement('script');
+                script.src = 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js';
+                script.onload = resolve;
+                script.onerror = reject;
+                document.body.appendChild(script);
+            } else {
+                resolve();
+            }
+        });
+    }
+
+    render() {
+        return <div id="map" style={{height: 300, width: "100%"}}></div>
+    }
+}
